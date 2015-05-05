8948a27e81920a4c6021999e47102b4520b31562
RAbramson1 <RAbramson1@users.noreply.github.com>
2015-05-05 12:25:21 -0400

    Update contacts.py
    Changed column order.

--- contacts.py
+++ contacts.py
@@ -13,7 +13,7 @@ outfile = csv.writer(open('sf_upload-contacts.csv', 'w'))
 
 # Headers for uploading new actions in SF
 new_headers = ['VANID', 'Result', 'Subject', 'Action_Date', 'Contact Method', 'Name ID', \
-	'Record Type ID', 'Priority', 'Status', 'Additional Comments']
+	'Additional Comments', 'Record Type ID', 'Priority', 'Status']
 added_fields = ['012i0000001Dqri', 'Normal', 'Completed']
 outfile.writerow(new_headers)
 
@@ -23,5 +23,5 @@ for row in infile:
 	if cols[4] not in ('Phone', 'Email', 'Facebook'):
 		continue
 	else:
-		new_row = cols[0:2] + cols[2] + cols[3:] + [get_sfid(cols[0])] + cols[2] + added_fields
+		new_row = cols[0:2] + [cols[2]] + cols[3:] + [get_sfid(cols[0])] + [cols[2]] + added_fields
 		outfile.writerow(new_row)
